359ff6a89e2bb45374ec15bae46a316c466d427e
snermolaev <snermolaev@yandex-team.com>
2023-12-29 06:09:53 +0300

    Revert commit rXXXXXX, fix for

--- devtools/ymake/macro_processor.cpp
+++ devtools/ymake/macro_processor.cpp
@@ -1243,6 +1243,7 @@ void TCommandInfo::FillAddCtx(const TYVar& var, const TVars& parentVars) {
 
         if (IsGlobalReservedVar(tokenName)) {
             GetAddCtx(var)->AddUniqueDep(EDT_Property, EMNT_Property, FormatProperty(NProps::USED_RESERVED_VAR, tokenName));
+            continue;
         }
 
         if (token.Flags.Get(EMF_Tool) && !token.IsOwnArg) {
