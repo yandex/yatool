7ccb3108b8301e97a1a2717bdd063292a0227434
svidyuk <svidyuk@yandex-team.com>
2024-02-26 10:52:59 +0300

    Allow to get VERSION value in module macros 91dd2b77274d2bc777ac6a43b7d6b31f878f3efe

--- devtools/ymake/makefile_loader.cpp
+++ devtools/ymake/makefile_loader.cpp
@@ -420,7 +420,7 @@ bool TDirParser::KnownStatement(const TStringBuf& name, const TVector<TStringBuf
             YConfErrPrecise(Syntax, GetStatementRow(name), GetStatementColumn(name)) << "Empty " << name << " list is prohibited"  << Endl;
         }
         Owners.insert(args.begin(), args.end());
-    } else if ((name == "VERSION") || (name == "ORIGINAL_SOURCE") || (name == "NEED_REVIEW") || (name == "NEED_CHECK") || (name == "NO_NEED_CHECK")) {
+    } else if ((name == "ORIGINAL_SOURCE") || (name == "NEED_REVIEW") || (name == "NEED_CHECK") || (name == "NO_NEED_CHECK")) {
         // No operations.
     } else if (name == "ONLY_TAGS" || name == "EXCLUDE_TAGS" || name == "INCLUDE_TAGS") {
           if (SubModules.empty()) {
