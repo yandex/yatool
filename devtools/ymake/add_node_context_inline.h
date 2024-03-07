91dbee8140a9314749d525829075edd314d045d3
somov <somov@yandex-team.com>
2024-03-07 12:08:10 +0300

    Clarify TNodeAddCtx::AddOutput eb923eb4ec1c3311d295cdc9498c02e81dfd57b0

--- devtools/ymake/add_node_context_inline.h
+++ devtools/ymake/add_node_context_inline.h
@@ -36,25 +36,33 @@ Y_FORCE_INLINE void TNodeAddCtx::AddDepsUnique(const TPropsNodeList& what, EDepT
 
 Y_FORCE_INLINE TAddDepAdaptor& TNodeAddCtx::AddOutput(ui64 elemId, EMakeNodeType defaultType, bool addToOwn) {
     auto i = UpdIter.Nodes.Insert(MakeDepsCacheId(defaultType, elemId), &YMake, Module);
-    TNodeAddCtx& add = *i->second.AddCtx;
+    TUpdEntryStats& nodeData = i->second;
+
+    TNodeAddCtx& add = *nodeData.AddCtx;
     if (add.NodeType == EMNT_Deleted) {
         add.NodeType = defaultType;
         add.ElemId = elemId;
     }
 
-    i->second.SetReassemble(true); // note: this is for output files only, commands reassemble based on CmdOrigin
+    // This is for output files only, commands reassemble based on CmdOrigin.
+    nodeData.SetReassemble(true);
+
     if (addToOwn) {
         Y_ASSERT(UseFileId(defaultType));
+
         TModAddData& modData = add.GetModuleData();
         modData.AlreadyAdded = !Module->AddEntry(elemId);
         modData.Added = true;
-        i->second.OnceEntered = false;
-        if (i->second.AddCtx && !i->second.AddCtx->NeedInit2) {
-            // This was already processed, thus unusable
-            Y_ASSERT(!i->second.AddCtxOwned);
-            i->second.AddCtx = nullptr;
+        nodeData.OnceEntered = false;
+
+        if (!nodeData.AddCtx->NeedInit2) {
+            // This was already processed, thus unusable.
+            // Though we will return "add" (and use) from here.
+            Y_ASSERT(!nodeData.AddCtxOwned);
+            nodeData.AddCtx = nullptr;
         }
     }
+
     return add;
 }
 
