113b9d0ebeb40b019421bb67bd1313eb7e3bcdd8
somov <somov@yandex-team.com>
2024-07-30 01:42:50 +0300

    Remove old UIDs: part 6 3ea47391f7150a181d004118357e3286ef39353e

--- devtools/ymake/json_entry_stats.cpp
+++ devtools/ymake/json_entry_stats.cpp
@@ -2,128 +2,58 @@
 
 #include "json_saveload.h"
 
-TJsonStatsBase::~TJsonStatsBase() noexcept {
-}
-
-TJsonStatsOld::TJsonStatsOld(TNodeDebugOnly nodeDebug)
-    : ContextSign{nodeDebug, "TJSONEntryStats::ContextSign"sv}
-    , IncludedContextSign{nodeDebug, "TJSONEntryStats::IncludedContextSign"sv}
-    , SelfContextSign{nodeDebug, "TJSONEntryStats::SelfContextSign"sv}
-    , IncludedSelfContextSign{nodeDebug, "TJSONEntryStats::IncludedSelfContextSign"sv}
-    , RenderId{nodeDebug, "TJSONEntryStats::RenderId"sv}
-{
-}
-
-void TJsonStatsOld::SetContextSign(const TMd5SigValue& md5, TUidDebugNodeId id) {
-    ContextSign = md5;
-    if (Y_UNLIKELY(Diag()->UIDs)) {
-        NUidDebug::LogContextMd5Assign(id, GetNodeUid());
-    }
-    YDIAG(Dev) << "Set ContextSign, value is " << ContextSign.ToBase64() << Endl;
-}
-
-void TJsonStatsOld::SetContextSign(const TMd5Value& oldMd5, TUidDebugNodeId id, TStringBuf contextSalt) {
-    TMd5Value md5 = oldMd5;
-    md5.Update(contextSalt, "TJSONEntryStats::SetContextSign::<contextSalt>"sv);
-    ContextSign.MoveFrom(std::move(md5));
-    if (Y_UNLIKELY(Diag()->UIDs)) {
-        NUidDebug::LogContextMd5Assign(id, GetNodeUid());
-    }
-    YDIAG(Dev) << "Set ContextSign, value is " << ContextSign.ToBase64() << Endl;
-}
-
-void TJsonStatsOld::SetIncludedContextSign(const TMd5SigValue& md5) {
-    IncludedContextSign = md5;
-    YDIAG(Dev) << "Set IncludedContextSign, value is " << IncludedContextSign.ToBase64() << Endl;
-}
-
-void TJsonStatsOld::SetIncludedContextSign(const TMd5Value& oldMd5) {
-    IncludedContextSign.CopyFrom(oldMd5);
-    YDIAG(Dev) << "Set IncludedContextSign, value is " << IncludedContextSign.ToBase64() << Endl;
-}
-
-void TJsonStatsOld::SetSelfContextSign(const TMd5Value& oldMd5, TUidDebugNodeId id, TStringBuf contextSalt) {
-    TMd5Value md5 = oldMd5;
-    md5.Update(contextSalt, "TJSONEntryStats::SetSelfContextSign::<contextSalt>"sv);
-    SelfContextSign.MoveFrom(std::move(md5));
-    if (Y_UNLIKELY(Diag()->UIDs)) {
-        NUidDebug::LogSelfContextMd5Assign(id, GetNodeUid());
-    }
-    YDIAG(Dev) << "Set SelfContextSign, value is " << SelfContextSign.ToBase64() << Endl;
-}
-
-void TJsonStatsOld::SetSelfContextSign(const TMd5SigValue& md5, TUidDebugNodeId id) {
-    SelfContextSign = md5;
-    if (Y_UNLIKELY(Diag()->UIDs)) {
-        NUidDebug::LogSelfContextMd5Assign(id, GetNodeUid());
-    }
-    YDIAG(Dev) << "Set SelfContextSign, value is " << SelfContextSign.ToBase64() << Endl;
-}
-
-TJsonStatsNew::TJsonStatsNew(TNodeDebugOnly nodeDebug)
-    : StructureUID(nodeDebug, "TJSONEntryStats::StructureUID"sv)
-    , PreStructureUID(nodeDebug, "TJSONEntryStats::PreStructureUID"sv)
-    , IncludeStructureUID(nodeDebug, "TJSONEntryStats::IncludeStructureUID"sv)
-    , ContentUID(nodeDebug, "TJSONEntryStats::ContentUID"sv)
-    , IncludeContentUID(nodeDebug, "TJSONEntryStats::IncludeContentUID"sv)
-    , FullUID(nodeDebug, "TJSONEntryStats::FullUID"sv)
-    , SelfUID(nodeDebug, "TJSONEntryStats::SelfUID"sv)
-    , IsFullUIDCompleted(false)
-    , IsSelfUIDCompleted(false)
-{}
-
-void TJsonStatsNew::SetStructureUid(const TMd5SigValue& md5) {
+void TJSONEntryStats::SetStructureUid(const TMd5SigValue& md5) {
     StructureUID = md5;
     YDIAG(Dev) << "Set StructureUID, value is " << StructureUID.ToBase64() << Endl;
 }
 
-void TJsonStatsNew::SetStructureUid(const TMd5Value& oldMd5) {
+void TJSONEntryStats::SetStructureUid(const TMd5Value& oldMd5) {
     StructureUID.CopyFrom(oldMd5);
     YDIAG(Dev) << "Set StructureUID, value is " << StructureUID.ToBase64() << Endl;
 }
 
-void TJsonStatsNew::SetIncludeStructureUid(const TMd5SigValue& md5) {
+void TJSONEntryStats::SetIncludeStructureUid(const TMd5SigValue& md5) {
     IncludeStructureUID = md5;
     YDIAG(Dev) << "Set IncludeStructureUID, value is " << IncludeStructureUID.ToBase64() << Endl;
 }
 
-void TJsonStatsNew::SetIncludeStructureUid(const TMd5Value& oldMd5) {
+void TJSONEntryStats::SetIncludeStructureUid(const TMd5Value& oldMd5) {
     IncludeStructureUID.CopyFrom(oldMd5);
 }
 
-void TJsonStatsNew::SetContentUid(const TMd5SigValue& md5) {
+void TJSONEntryStats::SetContentUid(const TMd5SigValue& md5) {
     ContentUID = md5;
     YDIAG(Dev) << "Set ContentUID, value is " << ContentUID.ToBase64() << Endl;
 }
 
-void TJsonStatsNew::SetContentUid(const TMd5Value& oldMd5) {
+void TJSONEntryStats::SetContentUid(const TMd5Value& oldMd5) {
     ContentUID.CopyFrom(oldMd5);
     YDIAG(Dev) << "Set ContentUID, value is " << ContentUID.ToBase64() << Endl;
 }
 
-void TJsonStatsNew::SetIncludeContentUid(const TMd5SigValue& md5) {
+void TJSONEntryStats::SetIncludeContentUid(const TMd5SigValue& md5) {
     IncludeContentUID = md5;
     YDIAG(Dev) << "Set IncludeContentUID, value is " << IncludeContentUID.ToBase64() << Endl;
 }
 
-void TJsonStatsNew::SetIncludeContentUid(const TMd5Value& oldMd5) {
+void TJSONEntryStats::SetIncludeContentUid(const TMd5Value& oldMd5) {
     IncludeContentUID.CopyFrom(oldMd5);
     YDIAG(Dev) << "Set ContentUID, value is " << IncludeContentUID.ToBase64() << Endl;
 }
 
-void TJsonStatsNew::SetFullUid(const TMd5Value& oldMd5) {
+void TJSONEntryStats::SetFullUid(const TMd5Value& oldMd5) {
     FullUID.CopyFrom(oldMd5);
     IsFullUIDCompleted = true;
     YDIAG(Dev) << "Set FullUID, value is " << FullUID.ToBase64() << Endl;
 }
 
-void TJsonStatsNew::SetFullUid(const TMd5SigValue& oldMd5) {
+void TJSONEntryStats::SetFullUid(const TMd5SigValue& oldMd5) {
     FullUID = oldMd5;
     IsFullUIDCompleted = true;
     YDIAG(Dev) << "Set FullUID, value is " << FullUID.ToBase64() << Endl;
 }
 
-void TJsonStatsNew::SetSelfUid(const TMd5Value& oldMd5) {
+void TJSONEntryStats::SetSelfUid(const TMd5Value& oldMd5) {
     SelfUID.CopyFrom(oldMd5);
     IsSelfUIDCompleted = true;
     YDIAG(Dev) << "Set SelfUID, value is " << SelfUID.ToBase64() << Endl;
@@ -135,13 +65,21 @@ void TJsonDeps::TraceAdd(TNodeId id) {
 
 TJSONEntryStats::TJSONEntryStats(TNodeDebugOnly nodeDebug, bool inStack, bool isFile)
     : TEntryStats(nodeDebug, inStack, isFile)
-    , TJsonStatsNew(nodeDebug)
     , TNodeDebugOnly{nodeDebug}
     , AllFlags(0)
     , IncludedDeps(nodeDebug, "IncludedDeps")
     , NodeDeps(nodeDebug, "NodeDeps")
     , NodeToolDeps(nodeDebug, "NodeToolDeps")
     , IsGlobalVarsCollectorStarted(false)
+    , StructureUID(nodeDebug, "TJSONEntryStats::StructureUID"sv)
+    , PreStructureUID(nodeDebug, "TJSONEntryStats::PreStructureUID"sv)
+    , IncludeStructureUID(nodeDebug, "TJSONEntryStats::IncludeStructureUID"sv)
+    , ContentUID(nodeDebug, "TJSONEntryStats::ContentUID"sv)
+    , IncludeContentUID(nodeDebug, "TJSONEntryStats::IncludeContentUID"sv)
+    , FullUID(nodeDebug, "TJSONEntryStats::FullUID"sv)
+    , SelfUID(nodeDebug, "TJSONEntryStats::SelfUID"sv)
+    , IsFullUIDCompleted(false)
+    , IsSelfUIDCompleted(false)
 {
 }
 
--- devtools/ymake/json_entry_stats.h
+++ devtools/ymake/json_entry_stats.h
@@ -18,87 +18,88 @@ struct TJsonStackData {
 
 using TJsonStateItem = TGraphIteratorStateItem<TJsonStackData, true>;
 
-class TJsonStatsOld;
-class TJsonStatsNew;
+struct TJsonDepsDebug : public TNodeDebug {
+    const char* DebugName;
 
-class TJsonStatsBase {
-public:
-    virtual ~TJsonStatsBase() noexcept;
-
-    inline TJsonStatsOld* Old() noexcept;
-    inline const TJsonStatsOld* Old() const noexcept;
-
-    inline TJsonStatsNew* New() noexcept;
-    inline const TJsonStatsNew* New() const noexcept;
+    TJsonDepsDebug(const TNodeDebug& nodeDebug, const char* debugName)
+        : TNodeDebug(nodeDebug)
+        , DebugName(debugName)
+    {
+    }
 };
 
-class TJsonStatsOld : public TJsonStatsBase {
-public:
-    TJsonStatsOld(TNodeDebugOnly nodeDebug);
+using TJsonDepsDebugOnly = TDebugOnly<TJsonDepsDebug>;
 
-    const TMd5SigValue& GetContextSign() const {
-        return ContextSign;
+struct TJsonDeps :
+    public THolder<TUniqVector<TNodeId>>,
+    public TJsonDepsDebugOnly
+{
+    using TJsonDepsDebugOnly::TJsonDepsDebugOnly;
+
+    void Add(TNodeId id) {
+        AddTo(id, *this);
+
+        if constexpr (DebugEnabled) {
+            TraceAdd(id);
+        }
     }
 
-    const TMd5SigValue& GetIncludedContextSign() const {
-        return IncludedContextSign;
+    void Add(const TJsonDeps& ids) {
+        AddTo<TUniqVector<TNodeId>>(ids, *this);
+
+        if constexpr (DebugEnabled) {
+            if (ids) {
+                for (TNodeId id : *ids) {
+                    TraceAdd(id);
+                }
+            }
+        }
     }
 
-    const TMd5SigValue& GetSelfContextSign() const {
-        return SelfContextSign;
-    }
-
-    const TMd5SigValue& GetIncludedSelfContextSign() const {
-        return IncludedSelfContextSign;
-    }
-
-    const TMd5SigValue& GetRenderId() const {
-        return RenderId;
-    }
-
-    TString GetNodeUid() const {
-        return ContextSign.ToBase64();
-    }
-
-    TString GetNodeSelfUid() const {
-        return SelfContextSign.ToBase64();
-    }
-
-    void SetContextSign(const TMd5SigValue& md5, TUidDebugNodeId id);
-    void SetContextSign(const TMd5Value& oldMd5, TUidDebugNodeId id, TStringBuf contextSalt);
-
-    void SetIncludedContextSign(const TMd5SigValue& md5);
-    void SetIncludedContextSign(const TMd5Value& oldMd5);
-
-    void SetSelfContextSign(const TMd5SigValue& md5, TUidDebugNodeId id);
-    void SetSelfContextSign(const TMd5Value& oldMd5, TUidDebugNodeId id, TStringBuf contextSalt);
-
-    void SetIncludedSelfContextSign(const TMd5Value& oldMd5) {
-        IncludedSelfContextSign.CopyFrom(oldMd5);
-    }
-
-    void SetIncludedSelfContextSign(const TMd5SigValue& md5) {
-        IncludedSelfContextSign = md5;
-    }
-
-    void SetRenderId(const TMd5SigValue& md5, TUidDebugNodeId) {
-        ContextSign = md5;
-    }
-
-    void SetRenderId(const TMd5Value& oldMd5, TUidDebugNodeId) {
-        RenderId.CopyFrom(oldMd5);
-    }
-
-    TMd5SigValue ContextSign;
-    TMd5SigValue IncludedContextSign;
-    TMd5SigValue SelfContextSign; // Without Deps content
-    TMd5SigValue IncludedSelfContextSign;
-    TMd5SigValue RenderId; // First part of PartialMatchKey in JSON-caching
+private:
+    void TraceAdd(TNodeId id);
 };
 
-class TJsonStatsNew : public TJsonStatsBase {
+struct TJSONEntryStats : public TEntryStats, public TNodeDebugOnly  {
+    union {
+        ui8 AllFlags;
+        struct {  // 7 bits used
+            bool IncludesMd5Started : 1;
+            bool HasUsualEntry : 1;
+            bool AddToOutputs : 1;
+            bool WasFresh : 1;
+            bool Fake : 1;
+
+            // Из-за использования кэша, присутствие TJSONEntryStats в Nodes
+            // больше не означает, что соответствующий узел был посещён.
+            // Теперь факт посещения обозначен этим флагом.
+            bool WasVisited : 1;
+
+            // Признак того, что все персистентные поля были вычислены
+            // или загружены из кэша;
+            bool Completed : 1;
+        };
+    };
+
+    TNodeId LoopId = 0;
+    TNodeId OutTogetherDependency = 0;
+
+    TJsonDeps IncludedDeps;
+    TJsonDeps NodeDeps;
+    TJsonDeps NodeToolDeps;
+    THolder<TUniqVector<TNodeId>> ExtraOuts;
+
+    THolder<THashSet<TString>> UsedReservedVars;
+    bool IsGlobalVarsCollectorStarted;
+
 public:
-    TJsonStatsNew(TNodeDebugOnly nodeDebug);
+    TJSONEntryStats(TNodeDebugOnly nodeDebug, bool inStack = false, bool isFile = false);
+
+    using TItemDebug = TNodeDebugOnly;
+
+    void Save(TSaveBuffer* buffer, const TDepGraph& graph) const noexcept;
+    void LoadStructureUid(TLoadBuffer* buffer, const TDepGraph& graph, bool asPre = false) noexcept;
+    bool Load(TLoadBuffer* buffer, const TDepGraph& graph) noexcept;
 
     void SetStructureUid(const TMd5SigValue& md5);
     void SetStructureUid(const TMd5Value& oldMd5);
@@ -188,126 +189,6 @@ public:
 
     bool IsFullUIDCompleted;
     bool IsSelfUIDCompleted;
-};
-
-inline TJsonStatsOld* TJsonStatsBase::Old() noexcept {
-    Y_ASSERT(dynamic_cast<TJsonStatsOld*>(this) != nullptr);
-    return static_cast<TJsonStatsOld*>(this);
-}
-
-inline const TJsonStatsOld* TJsonStatsBase::Old() const noexcept {
-    Y_ASSERT(dynamic_cast<const TJsonStatsOld*>(this) != nullptr);
-    return static_cast<const TJsonStatsOld*>(this);
-}
-
-inline TJsonStatsNew* TJsonStatsBase::New() noexcept {
-    Y_ASSERT(dynamic_cast<TJsonStatsNew*>(this) != nullptr);
-    return static_cast<TJsonStatsNew*>(this);
-}
-
-inline const TJsonStatsNew* TJsonStatsBase::New() const noexcept {
-    Y_ASSERT(dynamic_cast<const TJsonStatsNew*>(this) != nullptr);
-    return static_cast<const TJsonStatsNew*>(this);
-}
-
-struct TJSONEntryStatsNewUID : public TEntryStats, public TNodeDebugOnly {
-    TJSONEntryStatsNewUID(TNodeDebugOnly nodeDebug, bool inStack, bool isFile);
-
-
-protected:
-};
-
-struct TJsonDepsDebug : public TNodeDebug {
-    const char* DebugName;
-
-    TJsonDepsDebug(const TNodeDebug& nodeDebug, const char* debugName)
-        : TNodeDebug(nodeDebug)
-        , DebugName(debugName)
-    {
-    }
-};
-
-using TJsonDepsDebugOnly = TDebugOnly<TJsonDepsDebug>;
-
-struct TJsonDeps :
-    public THolder<TUniqVector<TNodeId>>,
-    public TJsonDepsDebugOnly
-{
-    using TJsonDepsDebugOnly::TJsonDepsDebugOnly;
-
-    void Add(TNodeId id) {
-        AddTo(id, *this);
-
-        if constexpr (DebugEnabled) {
-            TraceAdd(id);
-        }
-    }
-
-    void Add(const TJsonDeps& ids) {
-        AddTo<TUniqVector<TNodeId>>(ids, *this);
-
-        if constexpr (DebugEnabled) {
-            if (ids) {
-                for (TNodeId id : *ids) {
-                    TraceAdd(id);
-                }
-            }
-        }
-    }
-
-private:
-    void TraceAdd(TNodeId id);
-};
-
-struct TJSONEntryStats : public TEntryStats, public TJsonStatsNew, public TNodeDebugOnly  {
-    union {
-        ui8 AllFlags;
-        struct {  // 7 bits used
-            bool IncludesMd5Started : 1;
-            bool HasUsualEntry : 1;
-            bool AddToOutputs : 1;
-            bool WasFresh : 1;
-            bool Fake : 1;
-
-            // Из-за использования кэша, присутствие TJSONEntryStats в Nodes
-            // больше не означает, что соответствующий узел был посещён.
-            // Теперь факт посещения обозначен этим флагом.
-            bool WasVisited : 1;
-
-            // Признак того, что все персистентные поля были вычислены
-            // или загружены из кэша;
-            bool Completed : 1;
-        };
-    };
-
-    TNodeId LoopId = 0;
-    TNodeId OutTogetherDependency = 0;
-
-    TJsonDeps IncludedDeps;
-    TJsonDeps NodeDeps;
-    TJsonDeps NodeToolDeps;
-    THolder<TUniqVector<TNodeId>> ExtraOuts;
-    THolder<TJsonStatsBase> Uids;
-
-    THolder<THashSet<TString>> UsedReservedVars;
-    bool IsGlobalVarsCollectorStarted;
-
-public:
-    TJSONEntryStats(TNodeDebugOnly nodeDebug, bool inStack = false, bool isFile = false);
-
-    TJsonStatsNew* NewUids() noexcept {
-        return this;
-    }
-
-    const TJsonStatsNew* NewUids() const noexcept {
-        return this;
-    }
-
-    using TItemDebug = TNodeDebugOnly;
-
-    void Save(TSaveBuffer* buffer, const TDepGraph& graph) const noexcept;
-    void LoadStructureUid(TLoadBuffer* buffer, const TDepGraph& graph, bool asPre = false) noexcept;
-    bool Load(TLoadBuffer* buffer, const TDepGraph& graph) noexcept;
 
 private:
     void SaveStructureUid(TSaveBuffer* buffer, const TDepGraph& graph) const noexcept;
