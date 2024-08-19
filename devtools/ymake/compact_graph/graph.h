b71e54a95a9a13d9d2ecaab2f6c304769108d4f6
svidyuk <svidyuk@yandex-team.com>
2024-08-19 08:32:10 +0300

    Container wrapper to store per-node nodes_data
    It's indexed by TNodeId and transparently incapsulates all of the logic
    related to:
     * Index casting
     * Contract aboit invalid element associated with TNodeIdInvalid
     * NodeId bounds checking
    
    It's designed to replace direct deque/vectors in compact graph
    implementation and in loop detection.
    7b2632080cad3e3566cbf8f713b898ef9b4e1c87

--- devtools/ymake/compact_graph/graph.h
+++ devtools/ymake/compact_graph/graph.h
@@ -1,46 +1,12 @@
 #pragma once
 
-#include <util/generic/cast.h>
+#include "nodeid.h"
+
 #include <util/generic/vector.h>
 #include <util/generic/deque.h>
 #include <util/generic/hash.h>
 #include <util/generic/algorithm.h>
 #include <util/ysaveload.h>
-
-enum class TNodeId: ui32 {
-    Invalid = 0,
-    MinValid = 1
-};
-constexpr TNodeId& operator++ (TNodeId& id) noexcept {
-    id = TNodeId{ToUnderlying(id) + 1};
-    return id;
-}
-constexpr TNodeId operator++ (TNodeId& id, int) noexcept {
-    return std::exchange(id, TNodeId{ToUnderlying(id) + 1});
-}
-constexpr TNodeId& operator-- (TNodeId& id) noexcept {
-    id = TNodeId{ToUnderlying(id) - 1};
-    return id;
-}
-constexpr TNodeId operator-- (TNodeId& id, int) noexcept {
-    return std::exchange(id, TNodeId{ToUnderlying(id) - 1});
-}
-constexpr size_t AsIdx(TNodeId id) noexcept {
-    return static_cast<size_t>(id);
-}
-// TODO: Investigate if using default hash for enum affects performance. Switching
-// node id hash function from default for ui32 to default for enum breaks at least one
-// test which relies on iteration order over hash map:
-// https://a.yandex-team.ru/arcadia/devtools/ymake/compact_graph/iter_ut.cpp?rev=r10491100#L684
-// Most likely that it's badly written test but it is kept untouched during turning TNodeId
-// into strongly typed enum.
-template<>
-struct hash<TNodeId> {
-    size_t operator() (const TNodeId& k) const noexcept {
-        return hash<ui32>{}(ToUnderlying(k));
-    }
-};
-
 
 template <typename V>
 V Deleted();
