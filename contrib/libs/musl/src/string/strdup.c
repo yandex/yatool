7a69e75b6ba1d3c9387410ee7db67e7620c4aa21
robot-piglet <robot-piglet@yandex-team.com>
2024-09-19 13:06:12 +0300

    Intermediate changes commit_hash:b55cc4a102e6fee9cb231a8009ac9255cd2c8529

---
+++ contrib/libs/musl/src/string/strdup.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+
+char *strdup(const char *s)
+{
+	size_t l = strlen(s);
+	char *d = malloc(l+1);
+	if (!d) return NULL;
+	return memcpy(d, s, l+1);
+}
+
+weak_alias(strdup, __strdup);
