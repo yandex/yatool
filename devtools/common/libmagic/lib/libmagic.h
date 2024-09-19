25b2dae5292c4f27af787debaf295c427dde81e6
mr2dark <mr2dark@yandex-team.com>
2024-09-19 14:06:03 +0300

    Added the 'distbuild_tracing_context' parameter reset for GG. commit_hash:ea9222155c867e085977013fc24a302573823880

--- devtools/common/libmagic/lib/libmagic.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace NMagic {
-    bool IsDynLib(const char* filename);
-    bool IsElfExecutable(const char* filename);
-    bool IsElf(const char* filename, const char* substr = nullptr);
-}
