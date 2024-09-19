1c390b8cd3e56a217f217b81d40e3e3c728ac4e7
alevitskii <alevitskii@yandex-team.com>
2024-09-19 12:59:16 +0300

    Cpp config from fs commit_hash:e0c334739b4c825eacb0c3ca1bccc9d91692bdb4

--- devtools/ya/handlers/style/cpp_style.py
+++ devtools/ya/handlers/style/cpp_style.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import
-import json
 
-import six
-import yaml
+import json
 import logging
+import six
 import subprocess
+import yaml
 
 from exts import func
 
+import core.config
 import core.resource
 import yalibrary.tools
 from .state_helper import stop
@@ -16,11 +17,26 @@ from .state_helper import stop
 logger = logging.getLogger(__name__)
 
 
+CPP_DEFAULT_CONFIGS = 'build/config/tests/cpp_style/default_configs.json'
+DEFAULT_FORMATTER = 'clang_format'
+
+
+@func.lazy
+def get_config_path():
+    # type(str) -> dict
+    return core.config.config_from_arc_rel_path(CPP_DEFAULT_CONFIGS)
+
+
 @func.lazy
 def config():
-    style_config = core.resource.try_get_resource('config.clang-format')
-
-    return json.dumps(yaml.safe_load(style_config))
+    try:
+        config_file = get_config_path()[DEFAULT_FORMATTER]
+        with open(config_file) as afile:
+            return json.dumps(yaml.safe_load(afile))
+    except Exception as e:
+        logger.warning(f"Couldn't obtain config from fs due to error {e!r}, reading from memory")
+        style_config = core.resource.try_get_resource('config.clang-format')
+        return json.dumps(yaml.safe_load(style_config))
 
 
 def fix(data, args):
