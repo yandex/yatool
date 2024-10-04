6d1605753f07b799c170b69ce97f3911e05c0395
say <say@yandex-team.com>
2024-10-04 19:17:19 +0300

    Add type hints commit_hash:6a2f90c4f83eb4428e090d07e3eeaa56912fc240

--- devtools/ya/yalibrary/sjson/dumper.pyx
+++ devtools/ya/yalibrary/sjson/dumper.pyx
@@ -1,15 +1,16 @@
 import io
 import sys
+import typing
 
 cdef extern from "devtools/ya/yalibrary/streaming_json_dumper/lib/dump.h" namespace "NSJson":
     void DumpToStream(object obj, object stream) except *
 
 
-def dump(obj, stream):
+def dump(obj: typing.Any, stream: io.IOBase) -> None:
     DumpToStream(obj, stream)
 
 
-def dumps(obj):
+def dumps(obj: typing.Any) -> bytes:
     stream = io.BytesIO()
     dump(obj, stream)
     return stream.getvalue()
--- devtools/ya/yalibrary/sjson/loader.pyx
+++ devtools/ya/yalibrary/sjson/loader.pyx
@@ -3,16 +3,17 @@ from libcpp cimport bool
 import io
 import six
 import sys
+import typing
 
 cdef extern from "devtools/ya/yalibrary/sjson/lib/load.h" namespace "NSJson":
     object LoadFromStream(object stream, bool internKeys, bool internVals)
 
 
-def load(stream: io.RawIOBase, intern_keys=False, intern_vals=False):
+def load(stream: io.IOBase, intern_keys: bool = False, intern_vals: bool = False) -> typing.Any:
     return LoadFromStream(stream, intern_keys, intern_vals)
 
 
-def loads(s: str|bytes, intern_keys=False, intern_vals=False):
+def loads(s: str|bytes, intern_keys: bool = False, intern_vals: bool = False) -> typing.Any:
     if isinstance(s, str) and six.PY3:
         s = s.encode()
     stream = io.BytesIO(s)
