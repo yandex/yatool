0ec6fd379fc18a7e670a520ff5ad6034043ed9b3
zhukoff-pavel <zhukoff-pavel@yandex-team.com>
2024-10-18 22:53:23 +0300

    [devtools/ya/yalibrary/build_graph_cache/changelist_storage/__init__.py] randomize store name with a part of uuid commit_hash:ad65209ab7ab0ccdb418632682958f0116f6f241

--- devtools/ya/yalibrary/build_graph_cache/changelist_storage/__init__.py
+++ devtools/ya/yalibrary/build_graph_cache/changelist_storage/__init__.py
@@ -1,6 +1,11 @@
 import os
+import uuid
 
 import yalibrary.loggers.file_log as file_log
+import logging
+
+_UUID_LENGTH = 6
+logger = logging.getLogger(__name__)
 
 
 def with_changelist_storage(log_dir, run_uid):
@@ -11,12 +16,15 @@ def with_changelist_storage(log_dir, run_uid):
     log_chunk = chunks.get_or_create(file_log.format_date(now))
     store_path = os.path.join(
         log_chunk,
-        '.'.join([file_log.format_time(now), run_uid, 'changelists']),
+        '.'.join([file_log.format_time(now), run_uid, 'changelists', uuid.uuid4().hex[:_UUID_LENGTH]]),
     )
 
     os.makedirs(store_path, exist_ok=True)
 
     yield store_path
 
-    if not os.listdir(store_path):
-        os.rmdir(store_path)
+    try:
+        if not os.listdir(store_path):
+            os.rmdir(store_path)
+    except Exception as e:
+        logger.debug('Failed to remove changelist storage %s', store_path, exc_info=e)
