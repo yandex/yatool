696342a52aeeadc77d95d96c6b78b627ba742cc3
kuzmich321 <kuzmich321@yandex-team.com>
2024-11-27 11:43:51 +0300

    update file permissions after download commit_hash:64a607c4a6d5e953e60c0dbde985e4c3080c4934

--- devtools/ya/yalibrary/fetcher/resource_fetcher.py
+++ devtools/ya/yalibrary/fetcher/resource_fetcher.py
@@ -154,9 +154,9 @@ def _get_downloader(fetcher, parsed_uri, progress_callback, state, keep_director
             ufetcher.get_ufetcher(),
             what_to_download,
             progress_callback,
-            state,
             keep_directory_packed,
             resource_info if config.has_mapping() or parsed_uri.resource_type in ('http', 'docker') else None,
+            parsed_uri.resource_type,
         )
 
     try:
