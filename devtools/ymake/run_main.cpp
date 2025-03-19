52fdde1c020a87b6e3465c76ca328908060bd197
iniklyaev <iniklyaev@yandex-team.com>
2025-03-19 17:55:02 +0300

    accept multiple configs an run them serially commit_hash:0ae5b9b8fcc5611a603f5ee426b3d9cbcedc9814

--- devtools/ymake/run_main.cpp
+++ devtools/ymake/run_main.cpp
@@ -29,6 +29,26 @@ static void SigInt(int) {
     _Exit(BR_INTERRUPTED);
 }
 
+static TVector<TVector<const char*>> SplitMulticonfigCmdline(int argc, char** argv) {
+    TVector<TVector<const char*>> configs;
+
+    using namespace std::views;
+
+    // split by --conf-id then drop the very first part: it is the program name or the whole command line
+    for (const auto v : split(TVector<const char*>{argv, argv + argc}, TVector<TStringBuf>{"--conf-id"sv}) | drop(1)) {
+        TVector<const char*> config{argv[0]};
+        // (+ 1) means we omit conf-id value since we don't need it for now
+        config.insert(config.end(), v.begin() + 1, v.end());
+        configs.push_back(std::move(config));
+    }
+
+    if (configs.empty()) {
+        configs.push_back(TVector<const char*>(argv, argv + argc));
+    }
+
+    return configs;
+}
+
 using namespace NLastGetopt;
 
 int YMakeMain(int argc, char** argv) {
@@ -44,33 +64,37 @@ int YMakeMain(int argc, char** argv) {
 
     SetAsyncSignalHandler(SIGINT, SigInt);
 
-    TBuildConfiguration conf;
+    int ret_code = BR_OK;
 
-    try {
-        TOpts opts;
-        opts.ArgPermutation_ = REQUIRE_ORDER;
-        opts.AddHelpOption('?');
+    for (const auto& value : SplitMulticonfigCmdline(argc, argv)) {
+        TBuildConfiguration conf;
 
-        conf.AddOptions(opts);
+        try {
+            TOpts opts;
+            opts.ArgPermutation_ = REQUIRE_ORDER;
+            opts.AddHelpOption('?');
 
-        const TOptsParseResult res(&opts, argc, argv);
+            conf.AddOptions(opts);
 
-        // This calls FORCE_TRACE(U, NEvent::TStageStated("ymake run")); after tracing initialization
-        conf.PostProcess(res.GetFreeArgs());
-    } catch (const yexception& error) {
-        YErr() << error.what() << Endl;
-        return BR_FATAL_ERROR;
+            const TOptsParseResult res(&opts, value.size(), const_cast<const char**>(value.data()));
+
+            // This calls FORCE_TRACE(U, NEvent::TStageStated("ymake run")); after tracing initialization
+            conf.PostProcess(res.GetFreeArgs());
+        } catch (const yexception& error) {
+            YErr() << error.what() << Endl;
+            return BR_FATAL_ERROR;
+        }
+
+        try {
+            LockAllMemory(LockCurrentMemory);
+        } catch (const yexception&) {
+            YDebug() << "mlockall failed" << Endl;
+        }
+
+        FORCE_TRACE(U, NEvent::TStageStarted("ymake main"));
+        ret_code |= main_real(conf);
+        FORCE_TRACE(U, NEvent::TStageFinished("ymake main"));
     }
-
-    try {
-        LockAllMemory(LockCurrentMemory);
-    } catch (const yexception&) {
-        YDebug() << "mlockall failed" << Endl;
-    }
-
-    FORCE_TRACE(U, NEvent::TStageStarted("ymake main"));
-    const auto ret_code = main_real(conf);
-    FORCE_TRACE(U, NEvent::TStageFinished("ymake main"));
 
     return ret_code;
 }
