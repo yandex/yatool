1cb643c05e417c7b0b1de000ac004e458278b84e
snermolaev <snermolaev@yandex-team.com>
2025-04-09 06:38:57 +0300

    Update ImageVersion after rXXXXXX commit_hash:be5c9d53b01ddee004219654218707c4bd2fee3b

--- devtools/ymake/saveload.cpp
+++ devtools/ymake/saveload.cpp
@@ -24,7 +24,7 @@
 #include <util/system/fstat.h>
 
 namespace {
-    const ui64 ImageVersion = 48;
+    const ui64 ImageVersion = 49;
     const ui64 DMCacheVersion = 1;
 
     template <size_t HashSize>
