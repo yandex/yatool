fbd2b681a48fb02327709ab3a181b0f14f82bb9e
zhukoff-pavel <zhukoff-pavel@yandex-team.com>
2025-06-17 11:22:48 +0300

    [devtools/ya/core/yarg] Add help filtering by group commit_hash:424075c19d4cc42a57c90c5036fcf839794720e0

--- devtools/ya/core/yarg/help.py
+++ devtools/ya/core/yarg/help.py
@@ -9,16 +9,17 @@ from devtools.ya.core.yarg.consumers import Consumer  # noqa: F401
 from devtools.ya.core.yarg.consumers import ArgConsumer, get_consumer
 from devtools.ya.core.yarg.excs import BaseOptsFrameworkException
 from devtools.ya.core.yarg.help_level import HelpLevel
-from devtools.ya.core.yarg.hooks import UpdateValueHook
+from devtools.ya.core.yarg.hooks import UpdateValueHook, SetValueHook
 from devtools.ya.core.yarg.groups import Group  # noqa: F401
 from devtools.ya.core.yarg.groups import OPERATIONAL_CONTROL_GROUP, UNCATEGORIZED_GROUP
 from devtools.ya.core.yarg.options import Options  # noqa: F401
 
 
 class ShowHelpException(BaseOptsFrameworkException):
-    def __init__(self, help_level=HelpLevel.BASIC):
+    def __init__(self, help_level=HelpLevel.BASIC, help_search=None):
         super(ShowHelpException, self).__init__()
         self.help_level = HelpLevel(help_level)
+        self.help_search = help_search
 
 
 def format_usage(opt):
@@ -52,25 +53,38 @@ def iterate_subgroups(options):
         yield subgroup, list(args)
 
 
-def format_help(options, help_level=HelpLevel.BASIC):
-    # type: (Options, HelpLevel) -> str
+def format_help(options, help_level=HelpLevel.BASIC, search_query=None):
+    # type: (Options, HelpLevel, str) -> str
     result = ['Options:']
 
     for group, args in iterate_options(options):
         if not args:
             continue
 
+        if search_query:
+            if group:
+                show_full_group = search_query in group.name.lower()
+            else:
+                show_full_group = search_query in UNCATEGORIZED_GROUP.name.lower()
+        else:
+            show_full_group = True
+
         group_result = []
 
         for subgroup, args in iterate_subgroups(args):
             subgroup_result = []
+
+            show_full_subgroup = False
+            if subgroup is not None and search_query:
+                show_full_subgroup = search_query in subgroup.name.lower()
 
             level_groups = collections.defaultdict(list)
             for arg in args:
                 if arg.visible.value <= help_level.value:
                     formatted_info = arg.formatted(options)
                     if formatted_info:
-                        level_groups[arg.visible].append(formatted_info)
+                        if show_full_group or show_full_subgroup or search_query in formatted_info.lower():
+                            level_groups[arg.visible].append(formatted_info)
 
             for vis_level, descs in sorted(level_groups.items(), key=lambda x: x[0].value):
                 if vis_level != HelpLevel.BASIC:
@@ -101,20 +115,35 @@ def format_help(options, help_level=HelpLevel.BASIC):
 class ShowHelpOptions(Options):
     def __init__(self):
         self._print_help_level = 0
+        self._print_help_search = None
 
     @staticmethod
     def consumer():
-        return ArgConsumer(
-            ['-h', '--help'],
-            help='Print help. Use -hh for more options and -hhh for even more.',
-            hook=UpdateValueHook('_print_help_level', lambda x: x + 1),
-            group=OPERATIONAL_CONTROL_GROUP,
-            visible=HelpLevel.BASIC,
-        )
+        return [
+            ArgConsumer(
+                ['-h', '--help'],
+                help='Print help. Use -hh for more options and -hhh for even more.',
+                hook=UpdateValueHook('_print_help_level', lambda x: x + 1),
+                group=OPERATIONAL_CONTROL_GROUP,
+                visible=HelpLevel.BASIC,
+            ),
+            ArgConsumer(
+                ['--help-search'],
+                help='Search and print help for specific term. Higher visibility levels (-hh/-hhh) may yield more results.',
+                hook=SetValueHook(name='_print_help_search'),
+                group=OPERATIONAL_CONTROL_GROUP,
+                visible=HelpLevel.BASIC,
+            ),
+        ]
 
     def postprocess(self):
+        if self._print_help_search:
+            self._print_help_search = self._print_help_search.lower()
+
+        if self._print_help_search and self._print_help_level == 0:
+            self._print_help_level = 1
         if self._print_help_level:
-            raise ShowHelpException(self._print_help_level)
+            raise ShowHelpException(self._print_help_level, self._print_help_search)
 
 
 class UsageExample(object):
