a50350c816a466379c16d9fe5176e9dca28974b4
dimdim11 <dimdim11@yandex-team.com>
2025-11-26 07:10:55 +0300

    Removing lock files after release commit_hash:79163929634405b5c34992c8e53f162dbc46cf65

--- devtools/ya/ide/gradle/common.py
+++ devtools/ya/ide/gradle/common.py
@@ -15,12 +15,15 @@ class ExclusiveLock:
         self.success_locked: bool = False
 
     def acquire(self) -> bool:
+        if self.success_locked:
+            return False
         try:
             if self.lock.acquire():
                 self.success_locked = True
                 return True
         except Exception:
             pass  # acquire failed
+        self._remove_lock_file()
         return False
 
     def release(self) -> None:
@@ -28,3 +31,12 @@ class ExclusiveLock:
             return
         self.lock.release()
         self.success_locked = False
+        self._remove_lock_file()
+
+    def _remove_lock_file(self) -> None:
+        try:
+            lock_file = Path(self.lock.filename)
+            if lock_file.exists():
+                lock_file.unlink()
+        except Exception:
+            pass  # ignore any errors at removing lock file
