9595a8b3ff9c2777cddbe4c8cfe3b3164f3d61ec
dimdim11 <dimdim11@yandex-team.com>
2025-12-08 15:46:37 +0300

    Revert commit rXXXXXX, Prebuild java and foreign targets together commit_hash:5d595e356fb887e35f200f02520e726707b55203

--- devtools/ya/ide/gradle/build.py
+++ devtools/ya/ide/gradle/build.py
@@ -2,10 +2,9 @@ import logging
 from pathlib import Path
 
 from devtools.ya.core import yarg
-from devtools.ya.core.common_opts import CrossCompilationOptions
 from devtools.ya.build import build_opts, ya_make
 
-from devtools.ya.ide.gradle.common import YaIdeGradleException
+from devtools.ya.ide.gradle.common import tracer, YaIdeGradleException
 from devtools.ya.ide.gradle.config import _JavaSemConfig
 from devtools.ya.ide.gradle.graph import _JavaSemGraph
 
@@ -21,7 +20,7 @@ class _Builder:
     def build(self) -> None:
         """Extract build targets from sem-graph and build they"""
         try:
-            java_rel_targets: list[Path] = list(set(self.sem_graph.get_run_java_program_rel_targets()))
+            build_rel_targets: list[Path] = list(set(self.sem_graph.get_run_java_program_rel_targets()))
             proto_rel_targets: list[Path] = []
             rel_targets = self.sem_graph.get_rel_targets()
             for rel_target, consumer_type, module_type in rel_targets:
@@ -43,94 +42,58 @@ class _Builder:
                     proto_rel_targets.append(rel_target)
                 else:
                     # Collect other targets
-                    java_rel_targets.append(rel_target)
+                    build_rel_targets.append(rel_target)
         except Exception as e:
             raise YaIdeGradleException(
                 f'Fail extract build targets from sem-graph {self.sem_graph.sem_graph_file}: {e}'
             ) from e
 
-        self._build_rel_targets(
-            java_rel_targets,
-            proto_rel_targets,
-            self.sem_graph.foreign_targets if self.config.params.build_foreign else [],
-        )
+        if build_rel_targets:
+            with tracer.scope('export & build>||build>java'):
+                self._build_rel_targets(build_rel_targets, proto_rel_targets)
+
+        if self.config.params.build_foreign and self.sem_graph.foreign_targets:
+            with tracer.scope('export & build>||build>foreign'):
+                self._build_rel_targets(self.sem_graph.foreign_targets, build_all_langs=True)
 
     def _build_rel_targets(
-        self, java_rel_targets: list[Path], proto_rel_targets: list[Path] = None, foreign_rel_targets: list[Path] = None
+        self, build_rel_targets: list[Path], proto_rel_targets: list[Path] = None, build_all_langs: bool = False
     ) -> None:
-        """Build all relative targets as one requests"""
-
-        if not java_rel_targets and not proto_rel_targets and not foreign_rel_targets:
-            return  # Nothing to build
-
+        """Build all relative targets by one graph, build_all_langs control only java targets or all languages targets"""
         import app_ctx
 
         try:
-            ya_make_opts = yarg.merge_opts(build_opts.ya_make_options(build_type='release', free_build_targets=True))
-            params = yarg.merge_params(ya_make_opts.initialize([]))
-            params.bld_dir = self.config.params.bld_dir
-            params.arc_root = str(self.config.arcadia_root)
-            params.bld_root = self.config.params.bld_root
-            params.ignore_recurses = True
-            params.build_graph_cache_force_local_cl = (
-                True  # workaround for local changelist (enabled by default, but not work here)
-            )
-            params.ymake_internal_servermode = (
-                False  # workaround for Error[ToolConf]: in ...: Tool is not found in a host graph
-            )
-            params.rel_targets = (
-                []  # workaround for Error: Failed in build process: 'Params' object has no attribute 'rel_targets'
-            )
-            # workaround get few changelists for ymakes at the same time, else some ymakes without changelist
-            params.build_graph_arc_command_timeout = 5
-            params.build_graph_arc_commit_lock_timeout = 5
-
-            build_infos = []
-
-            if proto_rel_targets:
-                proto_rel_targets = list(set([str(proto_rel_target) for proto_rel_target in proto_rel_targets]))
-                java_rel_targets += proto_rel_targets
-
-            if java_rel_targets:
-                java_rel_targets = list(set([str(java_rel_target) for java_rel_target in java_rel_targets]))
-                java_rel_targets.sort()
-                CrossCompilationOptions.PlatformSetAppendHook(
-                    'target_platforms',
-                    values=CrossCompilationOptions.generate_target_platforms_cxx,
-                    transform=CrossCompilationOptions.make_platform,
-                )(params, 'default')
-                CrossCompilationOptions.PlatformsSetExtraConstParamHook('build_type', 'release')(params)
-                CrossCompilationOptions.PlatformsSetExtraConstParamHook('ignore_recurses', True)(params)
-                for flag in self.config.params.ya_make_extra + ['-DSOURCES_JAR=yes']:
-                    CrossCompilationOptions.PlatformsSetExtraDictParamHook('target_platform_flag', 'flags', 'yes')(
-                        params, flag[2:]
-                    )
-                for java_rel_target in java_rel_targets:
-                    CrossCompilationOptions.PlatformsSetExtraAppendParamHook('target_platform_target', 'targets')(
-                        params, java_rel_target
-                    )
-                build_infos.append(
-                    f"{len(java_rel_targets)} {'(include ' + str(len(proto_rel_targets)) + ' proto)' if proto_rel_targets else ''} java targets"
+            if build_all_langs:
+                ya_make_opts = yarg.merge_opts(
+                    build_opts.ya_make_options(build_type='release', free_build_targets=True)
                 )
+                opts = yarg.merge_params(ya_make_opts.initialize([]))
+            else:
+                ya_make_opts = yarg.merge_opts(
+                    build_opts.ya_make_options(build_type='release', free_build_targets=True)
+                )
+                opts = yarg.merge_params(
+                    ya_make_opts.initialize(self.config.params.ya_make_extra + ['-DSOURCES_JAR=yes'])
+                )
+                if proto_rel_targets:
+                    build_rel_targets += proto_rel_targets
 
-            if foreign_rel_targets:
-                foreign_rel_targets = list(set([str(foreign_rel_target) for foreign_rel_target in foreign_rel_targets]))
-                foreign_rel_targets.sort()
-                CrossCompilationOptions.PlatformSetAppendHook(
-                    'target_platforms',
-                    values=CrossCompilationOptions.generate_target_platforms_cxx,
-                    transform=CrossCompilationOptions.make_platform,
-                )(params, 'default')
-                CrossCompilationOptions.PlatformsSetExtraConstParamHook('build_type', 'release')(params)
-                CrossCompilationOptions.PlatformsSetExtraConstParamHook('ignore_recurses', True)(params)
-                for foreign_rel_target in foreign_rel_targets:
-                    CrossCompilationOptions.PlatformsSetExtraAppendParamHook('target_platform_target', 'targets')(
-                        params, foreign_rel_target
-                    )
-                build_infos.append(f"{len(foreign_rel_targets)} foreign targets")
+            opts.bld_dir = self.config.params.bld_dir
+            opts.arc_root = str(self.config.arcadia_root)
+            opts.bld_root = self.config.params.bld_root
+            opts.ignore_recurses = True
 
-            builder = ya_make.YaMake(params, app_ctx)
-            self.logger.info("Building %s...", " and ".join(build_infos))
+            opts.rel_targets = []
+            opts.abs_targets = []
+            build_rel_targets = list(set(build_rel_targets))
+            for build_rel_target in build_rel_targets:  # Add all targets for build simultaneously
+                opts.rel_targets.append(str(build_rel_target))
+                opts.abs_targets.append(str(self.config.arcadia_root / build_rel_target))
+
+            self.logger.info(
+                "Building %i %s targets...", len(build_rel_targets), "foreign" if build_all_langs else "java"
+            )
+            builder = ya_make.YaMake(opts, app_ctx)
             return_code = builder.go()
             if return_code != 0:
                 raise YaIdeGradleException('Some builds failed')
