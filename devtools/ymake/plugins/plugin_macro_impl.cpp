477f3cb4ae024c6c71969e64409c9718193786a8
svidyuk <svidyuk@yandex-team.com>
2025-12-29 06:34:01 +0300

    Reduce indentation level in the code commit_hash:4a8058f5563104ad0dc9aec59fcf74fbeb509fa1

--- devtools/ymake/plugins/plugin_macro_impl.cpp
+++ devtools/ymake/plugins/plugin_macro_impl.cpp
@@ -7,55 +7,54 @@
 
 #include <library/cpp/pybind/cast.h>
 
-namespace NYMake {
-    namespace NPlugins {
-        void TPluginMacroImpl::Execute(TPluginUnit& unit, const TVector<TStringBuf>& params) {
-            PyObject* tupleArgs = PyTuple_New(params.size() + 1);
+namespace NYMake::NPlugins {
+    void TPluginMacroImpl::Execute(TPluginUnit& unit, const TVector<TStringBuf>& params) {
+        PyObject* tupleArgs = PyTuple_New(params.size() + 1);
 
-            PyTuple_SetItem(tupleArgs, 0, CreateContextObject(&unit));
-            CheckForError();
+        PyTuple_SetItem(tupleArgs, 0, CreateContextObject(&unit));
+        CheckForError();
 
-            for (size_t i = 0; i < params.size(); ++i) {
-                PyTuple_SetItem(tupleArgs, i + 1, PyUnicode_FromString(TString{params[i]}.data()));
-            }
-
-            PyObject_CallObject(Obj_, tupleArgs);
-            CheckForError();
-            Py_DecRef(tupleArgs);
+        for (size_t i = 0; i < params.size(); ++i) {
+            PyTuple_SetItem(tupleArgs, i + 1, PyUnicode_FromString(TString{params[i]}.data()));
         }
 
-        TPluginMacroImpl::TPluginMacroImpl(PyObject* obj)
-            : Obj_(obj)
-        {
-            Py_XINCREF(Obj_);
+        PyObject_CallObject(Obj_, tupleArgs);
+        CheckForError();
+        Py_DecRef(tupleArgs);
+    }
+
+    TPluginMacroImpl::TPluginMacroImpl(PyObject* obj)
+        : Obj_(obj)
+    {
+        Py_XINCREF(Obj_);
+    }
+
+    TPluginMacroImpl::~TPluginMacroImpl() {
+        Py_XDECREF(Obj_);
+    }
+
+    void RegisterMacro(TBuildConfiguration& conf, const char* name, PyObject* func) {
+        if (!PyFunction_Check(func))
+            return;
+
+        PyCodeObject* code = (PyCodeObject*) PyFunction_GetCode(func);
+        TFsPath path = TFsPath(PyUnicode_AsUTF8(code->co_filename));
+
+        TString docText;
+        PyObject* doc = ((PyFunctionObject*)func)->func_doc;
+        if (PyUnicode_Check(doc)) {
+            docText = PyUnicode_AsUTF8(doc);
         }
 
-        TPluginMacroImpl::~TPluginMacroImpl() {
-            Py_XDECREF(Obj_);
-        }
-
-        void RegisterMacro(TBuildConfiguration& conf, const char* name, PyObject* func) {
-            if (PyFunction_Check(func)) {
-                PyCodeObject* code = (PyCodeObject*) PyFunction_GetCode(func);
-                TFsPath path = TFsPath(PyUnicode_AsUTF8(code->co_filename));
-
-                TString docText;
-                PyObject* doc = ((PyFunctionObject*)func)->func_doc;
-                if (PyUnicode_Check(doc)) {
-                    docText = PyUnicode_AsUTF8(doc);
-                }
-
-                auto macro = MakeSimpleShared<TPluginMacroImpl>(func);
-                macro->Definition = {
-                    std::move(docText),
-                    path.RelativePath(conf.SourceRoot),
-                    (size_t)code->co_firstlineno,
-                    1,
-                    (size_t)code->co_firstlineno,
-                    1
-                };
-                conf.RegisterPluginMacro(name, macro);
-            }
-        }
+        auto macro = MakeSimpleShared<TPluginMacroImpl>(func);
+        macro->Definition = {
+            std::move(docText),
+            path.RelativePath(conf.SourceRoot),
+            (size_t)code->co_firstlineno,
+            1,
+            (size_t)code->co_firstlineno,
+            1
+        };
+        conf.RegisterPluginMacro(name, macro);
     }
 }
