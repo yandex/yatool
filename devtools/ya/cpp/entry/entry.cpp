5b6ad00dd6009ae77e8c17142b31cc0610b1027b
alevitskii <alevitskii@yandex-team.com>
2026-02-19 16:38:24 +0300

    Revert commit rXXXXXX, Support response file in ya commit_hash:df81293bbf7c7ba17a8b1d6b4d599723d8bca974

--- devtools/ya/cpp/entry/entry.cpp
+++ devtools/ya/cpp/entry/entry.cpp
@@ -5,10 +5,7 @@
 #include <devtools/ya/cpp/lib/pgroup.h>
 #include <devtools/ya/cpp/entry/watchdog.h>
 
-#include <util/folder/path.h>
-#include "util/generic/fwd.h"
 #include <util/generic/hash.h>
-#include <util/stream/file.h>
 #include <util/system/env.h>
 
 namespace NYa {
@@ -23,39 +20,6 @@ namespace NYa {
         // Python3: https://a.yandex-team.ru/arcadia/library/python/runtime_py3/main/main.c?rev=f778e4e75ab5665f92abd1466726f4e5d51d4029#L231
         extern "C" TMain mainptr;
         TMain prevMainPtr;
-
-        TVector<TString> ExpandResponseFiles(int argc, char** argv) {
-            TVector<TString> result;
-            result.reserve(argc);
-
-            for (int i = 0; i < argc; ++i) {
-                TStringBuf arg(argv[i]);
-                if (arg.length() > 1 && arg[0] == '@' && arg[1] != '@') {
-                    TString filePath(arg.substr(1));
-                    TFsPath path(filePath);
-
-                    if (path.Exists()) {
-                        TFileInput input(filePath);
-                        TString line;
-                        while (input.ReadLine(line)) {
-                            if (!line.empty()) {
-                                result.push_back(std::move(line));
-                            }
-                        }
-                    } else {
-                        Cerr << "Response file '" << filePath << "' doesn't exist.\n";
-                        exit(1);
-                    }
-                } else if (arg.length() > 1 && arg.substr(0, 2) == "@@") {
-                    // Escaping: @@username -> @username
-                    result.emplace_back(arg.substr(1));
-                } else {
-                    result.emplace_back(arg);
-                }
-            }
-
-            return result;
-        }
 
         bool allowLogging(const IYaHandler *handlerPtr, const TVector<TStringBuf> args) {
             if (!handlerPtr->AllowLogging()) {
@@ -96,38 +60,29 @@ namespace NYa {
         }
 
         int Entry(int argc, char** argv) {
-            TVector<TString> expandedArgs = ExpandResponseFiles(argc, argv);
-            TVector<char*> expandedArgv;
-            expandedArgv.reserve(expandedArgs.size());
-            for (auto& arg : expandedArgs) {
-                expandedArgv.push_back(const_cast<char*>(arg.data()));
-            }
-            int expandedArgc = static_cast<int>(expandedArgv.size());
-            char** expandedArgvPtr = expandedArgv.data();
-
-            ::NYa::InitYt(expandedArgc, expandedArgvPtr);
-            auto newPgid = SetOwnProcessGroupId(expandedArgc, expandedArgvPtr);
+            ::NYa::InitYt(argc, argv);
+            auto newPgid = SetOwnProcessGroupId(argc, argv);
             InitWatchdogFromEnv();
 
             const auto factory = TSingletonClassFactory<IYaHandler>::Get();
             // Find tool name (first non-flag arg)
             const char* handlerName = nullptr;
             bool verbose = false;
-            for (int i = 1; i < expandedArgc; ++i) {
-                if (expandedArgs[i] == "-v" || expandedArgs[i] == "--verbose") {
+            for (int i = 1; i < argc; ++i) {
+                if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--verbose")) {
                     verbose = true;
                 }
-                if (expandedArgvPtr[i][0] != '-') {
-                    handlerName = expandedArgvPtr[i];
+                if (argv[i][0] != '-') {
+                    handlerName = argv[i];
                     break;
                 }
             }
             if (handlerName) {
                 if (IYaHandler* handlerPtr = factory->GetObjectPtr(handlerName)) {
                     TVector<TStringBuf> args;
-                    args.reserve(expandedArgc);
-                    for (int i = 0; i < expandedArgc; ++i) {
-                        args.push_back(expandedArgvPtr[i]);
+                    args.reserve(argc);
+                    for (int i = 0; i < argc; ++i) {
+                        args.push_back(argv[i]);
                     }
                     const IConfig& config = GetConfig();
                     InitLoggerRespectConfig(config.MiscRoot(), handlerPtr, args, TLOG_DEBUG, verbose);
@@ -142,10 +97,13 @@ namespace NYa {
                     DEBUG_LOG << "Fallback to python\n";
                 }
             }
-            return prevMainPtr(expandedArgc, expandedArgvPtr);
+            return prevMainPtr(argc, argv);
         }
 
         int InitEntry() {
+            if (GetEnv("DISABLE_YA_CPP_HANDLERS")) {
+                return 0;
+            }
             prevMainPtr = mainptr;
             mainptr = Entry;
             return 0;
