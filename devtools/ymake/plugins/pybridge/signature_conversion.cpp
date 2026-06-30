a6f5d69a387be8b8d889077cb8f0c19234718e72
svidyuk <svidyuk@yandex-team.com>
2026-06-30 10:36:44 +0300

    Support proper type hint on varaidic argument commit_hash:462a55a6c339b38123b00d457c48940fc2ff12d5

--- devtools/ymake/plugins/pybridge/signature_conversion.cpp
+++ devtools/ymake/plugins/pybridge/signature_conversion.cpp
@@ -55,13 +55,14 @@ std::expected<TSignature, ESignatureDeductionError> DeduceConfSignature(PyObject
         return std::unexpected(ESignatureDeductionError::IndistinguishableKwArg);
     auto kwargs = PyFunction_GetKwDefaults(&func);
 
-    const auto kwOnlyArgsNum = reinterpret_cast<PyCodeObject*>(PyFunction_GetCode(&func))->co_kwonlyargcount;
+    const auto& codeObj = *reinterpret_cast<PyCodeObject*>(PyFunction_GetCode(&func));
+    const auto kwOnlyArgsNum = codeObj.co_kwonlyargcount;
     const auto kwOnlyWithDefaultsNum = kwargs ? PyDict_GET_SIZE(kwargs) : 0;
     if (kwOnlyWithDefaultsNum != kwOnlyArgsNum) {
         return std::unexpected(ESignatureDeductionError::KwArgWithoutDefaults);
     }
 
-    bool varargFound = false;
+    const bool hasVararg = (codeObj.co_flags & CO_VARARGS);
     TVector<TString> positionals;
     TSignature::TKeywords keywords;
     while (PyDict_Next(signature, &pos, &key, &val)) {
@@ -83,13 +84,16 @@ std::expected<TSignature, ESignatureDeductionError> DeduceConfSignature(PyObject
             continue;
         }
 
+        const bool isVararg = hasVararg && pos == codeObj.co_argcount + 1;
         if (IsScalarArgTypeAnnotation(*val)) {
-            if (varargFound)
-                return std::unexpected(ESignatureDeductionError::PositionalAfterVararg);
-            positionals.push_back(TString{StrContent(*key)});
-        } else if (IsArrayArgTypeAnnotation(*val)) {
+            if (isVararg) {
+                positionals.push_back(TString{StrContent(*key)} + NStaticConf::ARRAY_SUFFIX);
+            } else {
+                positionals.push_back(TString{StrContent(*key)});
+            }
+        } else if (IsArrayArgTypeAnnotation(*val) && isVararg) {
+            // TODO(YMAKE-2151): remove support of `tuple[str, ...]` on `*args`
             positionals.push_back(TString{StrContent(*key)} + NStaticConf::ARRAY_SUFFIX);
-            varargFound = true;
         } else
             return std::unexpected(ESignatureDeductionError::WrongArgType);
     }
