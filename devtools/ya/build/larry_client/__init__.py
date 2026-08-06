c160469904924bc60276057b6306fe86ba512871
svidyuk <svidyuk@yandex-team.com>
2026-08-06 17:57:12 +0300

    Execute build with larry commit_hash:8be765c21e59efcc2f0d39ca6fd2be1e9a834d5e

--- devtools/ya/build/larry_client/__init__.py
+++ devtools/ya/build/larry_client/__init__.py
@@ -1,6 +1,41 @@
+import json
+import subprocess
+
+
 class LarryClient:
-    def __init__(self, callback):
+    def __init__(self, callback, graph):
         self.callback = callback
+        self.graph = graph
 
     def build(self, addr: str):
-        raise NotImplementedError
+        connection_type, target = self._parse_addr(addr)
+        if connection_type == 'sync':
+            return self._build_sync(target)
+
+        raise NotImplementedError('Connecting to Larry through a local socket is not implemented yet')
+
+    def _build_sync(self, binary_path: str):
+        process = subprocess.Popen(
+            [binary_path],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        )
+        process.communicate(json.dumps(self.graph).encode('utf-8'))
+        return {}, {}, None, {}, process.returncode, {}, {}, None, {}
+
+    def _parse_addr(self, addr: str):
+        if addr == 'sync':
+            return 'sync', self._get_latest_release()
+
+        connection_type, separator, target = addr.partition(':')
+        if separator and connection_type in ('local', 'sync') and target:
+            return connection_type, target
+
+        raise ValueError(
+            "Unsupported Larry address {!r}; expected 'local:<socket path>', "
+            "'sync', or 'sync:<binary path>'".format(addr)
+        )
+
+    @staticmethod
+    def _get_latest_release():
+        raise NotImplementedError('Getting the latest Larry release is not implemented yet')
